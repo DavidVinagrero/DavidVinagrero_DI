2bb74215d1f1d3cc5833a636d64d7cd8abcc9056
DavidVinagrero <david.i.vinagrero.e@gmail.com>
2022-09-30 10:07:04 +0200

    update

---
+++ T1/Ejercicio5/src/model/Carta.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Carta {
+    //Variables
+    final int valores[] = {1,2,3,4,5,6,7,10,11,12};
+    final String palos[]= {"Oros","Espadas","Bastos","Copas"};
+}
--- T1/Ejercicio5/src/model/carta.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import java.util.ArrayList;
-
-public class carta {
-    ArrayList<carta> cartas= new ArrayList<>(40);
-}
